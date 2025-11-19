fad1c6d8b7467de90c78a1d0b27c9b72b76df1b6
bestatclash <743376@student.dusd.net>
2025-11-19 13:50:17 -0800

    finished my scripts
    next class going to add charachter animation

--- P6CobianDylan2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P6CobianDylan2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour
 {
     
     public InputAction MoveAction;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
+       
         MoveAction.Enable();
     }
 
@@ -19,7 +21,7 @@ public class PlayerController : MonoBehaviour
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
 
-        Vector2 position = (Vector2)transform.position + move * 0.01f;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
        
         transform.position = position; 
 
